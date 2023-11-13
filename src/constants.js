45ca46b33c252cfbe917bc9015dda44324e30655
Cank256 <nkunzecaleb@gmail.com>
2023-11-13 15:31:37 +0000

    Deploying to gh-pages from @ Cank256/CubeFlyer@fed02129572f777c00eacd0ed3bef2159cc546d1 🚀

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,13 @@
 const gravity = new BABYLON.Vector2(0, -9.8);
 const flightForce = 5; // The amount of force applied when the "fly" button is pressed
 
-const greetings = ["It's a bird!", "It's a plane!", "1000% more Cube!", "A Whole New Dimension!"];
+const greetings = [
+    "It's a bird!",
+    "It's a plane!",
+    "1000% more Cube!",
+    "A Whole New Dimension!",
+    "Let's do this!",
+    "We are going to higher level!",
+    "Here we go again!",
+    "Yeeeehawww!!"
+];
